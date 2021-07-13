ea05f057a609af288f22d895b048e11e5a9c4ffc
AlexeyIK <oasisalex@gmail.com>
2021-07-14 01:09:34 +0300

    Читатель относительных размеров

---
+++ Color Matching/Helpers/ReadSizeExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ReadSizeExtension.swift
+//  Color Matching
+//
+//  Created by Alexey on 13.07.2021.
+//
+
+import SwiftUI
+
+struct ReadSizeExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReadSizeExtension_Previews: PreviewProvider {
+    static var previews: some View {
+        ReadSizeExtension()
+    }
+}
