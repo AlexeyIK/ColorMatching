d3dd568e8de84e87c405ca3413cf5d5d22dcd7e5
akuznetsov <akuznetsov@vizex.ru>
2021-08-07 18:51:50 +0300

    перенес анимацию кручения в отдельный файл

---
+++ Color Matching/Animations/RollingAnimation.swift
@@ -0,0 +1,37 @@
+//
+//  RollingAnimation.swift
+//  HueQueue
+//
+//  Created by Алексей Кузнецов on 07.08.2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct RollingModifier: AnimatableModifier {
+    var animatableData: Double {
+        get { percentage }
+        set {
+            percentage = newValue
+            checkIsFinish()
+        }
+    }
+    
+    var toAngle: Double = 0
+    var percentage: Double
+    var anchor: UnitPoint = .center
+    let onFinish: () -> ()
+    
+    func checkIsFinish() {
+        if (percentage == 1) {
+            DispatchQueue.main.async {
+                self.onFinish()
+            }
+        }
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(Angle(degrees: toAngle), anchor: anchor)
+    }
+}
