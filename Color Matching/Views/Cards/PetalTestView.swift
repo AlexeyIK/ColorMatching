5db67151fb35bbb5288ce3ba31a1c1f59f39e108
akuznetsov <akuznetsov@vizex.ru>
2021-08-07 18:52:13 +0300

    убрал откуда не надо

--- Color Matching/Views/Cards/PetalTestView.swift
+++ Color Matching/Views/Cards/PetalTestView.swift
@@ -51,36 +51,6 @@ struct PetalTestView: View {
     }
 }
 
-struct RollingModifier: AnimatableModifier {
-    
-    var animatableData: Double {
-        get { percentage }
-        set {
-            percentage = newValue
-            checkIsFinish()
-        }
-    }
-    
-    var toAngle: Double = 0
-    var percentage: Double
-    var anchor: UnitPoint = .center
-    let onFinish: () -> ()
-    
-    func checkIsFinish() {
-        if (percentage == 1) {
-            DispatchQueue.main.async {
-                self.onFinish()
-            }
-        }
-    }
-    
-    func body(content: Content) -> some View {
-        content
-            .rotationEffect(Angle(degrees: toAngle), anchor: anchor)
-      }
-    
-}
-
 struct PetalTestView_Previews: PreviewProvider {
     static var previews: some View {
         ZStack {
