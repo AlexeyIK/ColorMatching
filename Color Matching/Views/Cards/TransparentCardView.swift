ddafb938663bc86f330d85e597e058c3cf246eee
akuznetsov <alexkuz.av@gmail.com>
2021-11-09 22:13:34 +0300

    Полностью убрал прозрачность у карточек до тех пор, пока их не начнут сдвигать

--- Color Matching/Views/Cards/TransparentCardView.swift
+++ Color Matching/Views/Cards/TransparentCardView.swift
@@ -16,6 +16,7 @@ struct TransparentCardView: View {
     var drawShadow: Bool
     var showName: Bool
     var showColor: Bool = true
+    var transparencyOn: Bool = false
     var glowOffset: (CGSize, CGSize) = (CGSize(width: 0.9, height: 0.9), CGSize(width: 1.5, height: 1.5))
     
     var body: some View {
@@ -39,7 +40,7 @@ struct TransparentCardView: View {
                 )
                 // эффект блика
                 .overlay(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.init(red: 200, green: 200, blue: 200), Color.clear]), startPoint: UnitPoint(x: glowOffset.0.width, y: glowOffset.0.height), endPoint: UnitPoint(x: glowOffset.1.width, y: glowOffset.1.height)).blur(radius: 40).opacity(0.4).clipShape(RoundedRectangle(cornerRadius: 24)), alignment: .center)
-                .opacity(0.95)
+                .opacity(transparencyOn ? 0.95 : 1.0)
             
             if showName {
                 Text(String(colorModel.name != "" ? colorModel.name : colorModel.englishName))
@@ -59,7 +60,7 @@ struct TransparentCardView_Previews: PreviewProvider {
         ForEach(["iPhone 8", "iPhone Xs"], id: \.self) { device in
             ZStack {
                 BackgroundView()
-                TransparentCardView(colorModel: colorsData[310], drawBorder: true, drawShadow: true, showName: true)
+                TransparentCardView(colorModel: colorsData[310], drawBorder: true, drawShadow: true, showName: true, transparencyOn: false)
                     .previewDevice(PreviewDevice(stringLiteral: device))
                     .previewDisplayName(device)
             }
